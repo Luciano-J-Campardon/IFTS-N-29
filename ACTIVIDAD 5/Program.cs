2d735482ee04279bcb430b3d176a26a326837b72
Luciano Joaquín Campardón <argentinianlinuxnewbie@gmail.com>
2024-03-29 15:46:33 -0300

    ACTIVIDAD 5 - PRIMERA SEMANA DSOO

---
+++ ACTIVIDAD 5/Program.cs
@@ -0,0 +1,36 @@
+﻿int numSecuence;
+int position;
+int acumulator = 0;
+int numChar;
+string? numEnString = "";
+string arrayChar = "";
+bool validEntry = false;
+
+do
+{
+    Console.WriteLine("Ingrese un nùmero entero no negativo");
+    numEnString = Console.ReadLine();
+    if (numEnString != null && numEnString != "")
+    {
+        if ((validEntry = int.TryParse(numEnString, out numSecuence)) == false)
+        {
+            Console.WriteLine("El valor ingresado no es vàlido");
+        }
+        else
+        {
+            position = numEnString.Length;
+            string[] individualNumbers = new string[position];
+
+            for (int i = 0; i < position; i++)
+            {
+                arrayChar = numEnString.Substring(i, 1);
+                individualNumbers[i] = arrayChar;
+                int.TryParse(arrayChar, out numChar);
+                acumulator += numChar;
+            }
+
+            Console.WriteLine($"{string.Join(" + ", individualNumbers)} = {acumulator}");
+        }
+    }
+}
+while (validEntry != true);
