93d1697b1ec33799bd536974ddc1f9a1064f13bf
Luciano Joaquín Campardón <argentinianlinuxnewbie@gmail.com>
2024-03-29 15:40:45 -0300

    ACTIVIDAD 1 - PRIMERA SEMANA DSOO

---
+++ ACTIVIDAD 1/Program.cs
@@ -0,0 +1,21 @@
+﻿int precio;
+string? readResult = "";
+bool validEntry = false;
+
+do
+{
+    Console.WriteLine("Ingrese el precio del producto sin ganancia del negocio");
+    readResult = Console.ReadLine();
+    if (readResult != null && readResult != "")
+    {
+        if ((validEntry = int.TryParse(readResult, out precio)) == false)
+        {
+            Console.WriteLine("El valor ingresado no es vàlido");
+        }
+        else
+        {
+            Console.WriteLine($"El precio de venta del producto es de ${(precio*1.15)}");
+        }
+    }
+}
+while (validEntry != true);
